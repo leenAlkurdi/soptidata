1064a635a0491c9088909f6fc9fb6bea36ecba0a
Mohamad229 <mhmdhsn7752379@gmail.com>
2024-10-08 17:31:33 +0300

    Hasan

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./index.css"
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-300">Mohamad</h1>
+      <h1 className="text-red-300">Hasan</h1>
     </div>
   );
 }
