d90cac0349d62e58c2cfa0e1d94b736d5079404a
Qussay-bou <bouhadorq@gmail.com>
2024-10-08 17:27:37 +0300

    Sorry forgot to delete something <3

--- src/component/Lists.js
+++ src/component/Lists.js
@@ -2,22 +2,6 @@ import React from 'react'
 import jsondata from "../spotify_data.json"
 
 function Lists() {
-    const topPlay = () => {
-        let singers = jsondata.map(e => e["master_metadata_album_artist_name"])
-        let nodupsingers = singers.filter((value , index) =>singers.indexOf(value)===index && value !== null)
-        let singerplaytime =[]
-            nodupsingers.forEach(e => {
-              singerplaytime.push({
-                name: e,
-                playtime : 0 
-              })
-            })
-        singerplaytime.map(e => 
-          jsondata.map(item => e.name === item.master_metadata_album_artist_name ? e.playtime += item.ms_played: null))    
-        singerplaytime.sort((a,b) => b.playtime - a.playtime)
-        let TopArtists = singerplaytime.slice(0 , 100)
-        console.log(TopArtists);
-        }
         const toplist = (x) => {
             let item = jsondata.map(e => e[x])
             let nodup = item.filter((value , index) =>item.indexOf(value)===index && value !== null)
